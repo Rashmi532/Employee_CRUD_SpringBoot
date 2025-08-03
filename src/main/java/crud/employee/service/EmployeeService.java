19eeb8dcaf77f2cebf237ee5e9f44157f9cdb9e3
Rashmi532 <jsrashmi5@gmail.com>
2025-08-03 17:54:36 +0530

    Initial commit

---
+++ src/main/java/crud/employee/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package crud.employee.service;
+
+import java.util.List;
+
+import crud.employee.entity.Employee;
+
+public interface EmployeeService {
+void createNewEmployee(Employee emp);
+
+void updateEmployee(Employee emp);
+
+void deleteEmployee(Long id);
+
+Employee searchEmployee(Long id);
+
+List<Employee> getAllEmployees();
+}
